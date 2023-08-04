d33f96b9068cc5f7e279457ae3dd19b120d0bd36
Akash Sannasi <sannasi2@illinois.edu>
2023-08-04 18:57:06 -0500

    Reorganized and made cleaner readme

--- index.js
+++
@@ -1,8 +0,0 @@
-// functiondocument.getElementById("myButton").onclick = function(){
-//     var insertion = document.getElementById("myText").value;
-//     alert(insertion);
-// }
-function getInputValue() {
-    var insertion = document.getElementById("myInput").value;
-    alert(insertion);
-  }
